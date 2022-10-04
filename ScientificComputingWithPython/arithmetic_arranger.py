4d2227fd589c106e737e8567ea56ab71e266ea21
gabebierman <gabebierman@gmail.com>
2022-10-04 18:27:04 -0500

    initialize

---
+++ ScientificComputingWithPython/arithmetic_arranger.py
@@ -0,0 +1,20 @@
+# This entrypoint file to be used in development. Start by reading README.md
+# from pytest import main
+
+from arithmetic_arranger import arithmetic_arranger
+
+import re
+
+firstRow = []
+secondRow = []
+thirdRow = []
+solutionRow = []
+
+
+
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+# Run unit tests automatically
+# main(['-vv'])
